a6b768dd9b7cabc208f14b8319ae0a0a5a5830dc
Anatoliy Koperin <a.koperin@abbyy-ls.com>
2016-06-07 19:09:45 +0300

    fix stackoverflow

--- NConfiguration.Tests/Serialization/ChildDeserializerTests.cs
+++ NConfiguration.Tests/Serialization/ChildDeserializerTests.cs
@@ -17,12 +17,12 @@ namespace NConfiguration.Serialization
 @"<Root Text='textF1' Number='123' />".ToXmlView();
 
 			var cd = new ChildDeserializer(DefaultDeserializer.Instance);
-			cd.SetDeserializer((context, node) => context.Deserialize<int>(node) + 1);
+			cd.SetDeserializer((context, node) => context.Deserialize<string>(node) + "_1");
 
 			var item = cd.Deserialize<Test>(root);
 
-			Assert.That(item.Text, Is.EqualTo("textF1"));
-			Assert.That(item.Number, Is.EqualTo(124));
+			Assert.That(item.Text, Is.EqualTo("textF1_1"));
+			Assert.That(item.Number, Is.EqualTo(123));
 		}
 
 		[Test]
@@ -105,20 +105,19 @@ namespace NConfiguration.Serialization
 
 			Assert.That(item.Inner.Text, Is.EqualTo("itext_markS1_markS2"));
 			Assert.That(item.Inner.Number, Is.EqualTo(1301));
-
 		}
 
 		[Test]
 		public void InnerRecursiveOverrides()
 		{
 			var root =
-@"<Root Text='text' Number='1200'>
-	<RecursiveInner Text='itext' Number='1300'/>
+@"<Root Text='{$x}_{$y}' Number='1200'>
+	<RecursiveInner Text='i_{$x}_{$y}' Number='1300'/>
 </Root>
 ".ToXmlView();
 
 			var cd1 = new ChildDeserializer(DefaultDeserializer.Instance);
-			cd1.SetDeserializer((context, node) => context.Deserialize<string>(node).Replace("t", "xx"));
+			cd1.SetDeserializer((context, node) => context.Deserialize<string>(node).Replace("{$x}", "X"));
 			cd1.SetDeserializer((context, node) => context.Deserialize<int>(node) + 1);
 			cd1.SetDeserializer((context, node) =>
 			{
@@ -128,7 +127,7 @@ namespace NConfiguration.Serialization
 			});
 
 			var cd2 = new ChildDeserializer(cd1);
-			cd2.SetDeserializer((context, node) => context.Deserialize<string>(node).Replace("xxx", "O"));
+			cd2.SetDeserializer((context, node) => context.Deserialize<string>(node).Replace("{$y}", "Y"));
 			cd2.SetDeserializer((context, node) =>
 			{
 				var result = context.Deserialize<OuterTest>(node);
@@ -138,12 +137,72 @@ namespace NConfiguration.Serialization
 
 			var item = cd2.Deserialize<OuterTest>(root);
 
-			Assert.That(item.Text, Is.EqualTo("xxeO_markCh1_markCh2"));
+			Assert.That(item.Text, Is.EqualTo("X_Y_markCh1_markCh2"));
 			Assert.That(item.Number, Is.EqualTo(1201));
 
-			Assert.That(item.RecursiveInner.Text, Is.EqualTo("ixxeO_markCh1_markCh2"));
+			Assert.That(item.RecursiveInner.Text, Is.EqualTo("i_X_Y_markCh1_markCh2"));
 			Assert.That(item.RecursiveInner.Number, Is.EqualTo(1301));
+		}
 
+		[Test]
+		public void SpecialContextOverride()
+		{
+			var root =
+@"<Root Text='text_{$x}_{$y}_{$z}' Number='1200'>
+	<Inner Text='text_{$x}_{$y}_{$z}' Number='1300'/>
+	<RecursiveInner Text='text_{$x}_{$y}_{$z}' />
+</Root>
+".ToXmlView();
+
+			var cd1 = new ChildDeserializer(DefaultDeserializer.Instance);
+			cd1.SetDeserializer((context, node) => context.Deserialize<string>(node).Replace("{$x}", "X"));
+			cd1.SetDeserializer((context, node) =>
+			{
+				var newContext = new ChildDeserializer(context.Current);
+				newContext.SetDeserializer((context2, node2) => context2.Deserialize<string>(node2).Replace("{$y}", "Y"));
+				return context.Deserialize<Test>(newContext, node);
+			});
+
+			var cd2 = new ChildDeserializer(cd1);
+			cd2.SetDeserializer((context, node) => context.Deserialize<string>(node).Replace("{$z}", "Z"));
+
+			var item = cd2.Deserialize<OuterTest>(root);
+
+			Assert.That(item.Text, Is.EqualTo("text_X_{$y}_Z"));
+			Assert.That(item.Inner.Text, Is.EqualTo("text_X_Y_Z"));
+			Assert.That(item.RecursiveInner.Text, Is.EqualTo("text_X_{$y}_Z"));
+		}
+
+		[Test]
+		public void SpecialContextReplace()
+		{
+			var root =
+@"<Root Text='text_{$x}_{$z}' Number='100'>
+	<Inner Text='text_{$x}_{$z}' Number='100'/>
+	<RecursiveInner Text='text_{$x}_{$z}' Number='100'/>
+</Root>
+".ToXmlView();
+
+			var cdR = new ChildDeserializer(DefaultDeserializer.Instance);
+			cdR.SetDeserializer((context, node) => context.Deserialize<string>(node).Replace("{$x}", "Xr").Replace("{$z}", "Zr"));
+
+			var cd1 = new ChildDeserializer(DefaultDeserializer.Instance);
+			cd1.SetDeserializer((context, node) => context.Deserialize<int>(node) + 1);
+			cd1.SetDeserializer((context, node) => context.Deserialize<string>(node).Replace("{$x}", "X"));
+			cd1.SetDeserializer((context, node) => cdR.Deserialize<Test>(node));
+
+			var cd2 = new ChildDeserializer(cd1);
+			cd2.SetDeserializer((context, node) => context.Deserialize<string>(node).Replace("{$z}", "Z"));
+
+			var item = cd2.Deserialize<OuterTest>(root);
+
+			Assert.That(item.Text, Is.EqualTo("text_X_Z"));
+			Assert.That(item.Inner.Text, Is.EqualTo("text_Xr_Zr"));
+			Assert.That(item.RecursiveInner.Text, Is.EqualTo("text_X_Z"));
+
+			Assert.That(item.Number, Is.EqualTo(101));
+			Assert.That(item.Inner.Number, Is.EqualTo(100));
+			Assert.That(item.RecursiveInner.Number, Is.EqualTo(101));
 		}
 
 		public class Test
--- NConfiguration/Serialization/ChildDeserializer.cs
+++ NConfiguration/Serialization/ChildDeserializer.cs
@@ -6,10 +6,12 @@ using System.Collections.Concurrent;
 
 namespace NConfiguration.Serialization
 {
+	public delegate T ChildDeserializeDefinition<T>(ChildDeserializer.Context context, ICfgNode node);
+
 	public sealed class ChildDeserializer: IDeserializer
 	{
-		private IDeserializer _parent;
-		private Dictionary<Type, object> _funcMap = new Dictionary<Type, object>();
+		private readonly IDeserializer _parent;
+		private readonly Dictionary<Type, object> _funcMap = new Dictionary<Type, object>();
 
 		public ChildDeserializer(IDeserializer parent)
 		{
@@ -18,16 +20,38 @@ namespace NConfiguration.Serialization
 			_parent = parent;
 		}
 
+		public class Context
+		{
+			public IDeserializer Parent { get; private set; }
+			public IDeserializer Current { get; private set; }
+
+			internal Context(IDeserializer parent, IDeserializer current)
+			{
+				Parent = parent;
+				Current = current;
+			}
+
+			public T Deserialize<T>(ICfgNode node)
+			{
+				return Parent.Deserialize<T>(Current, node);
+			}
+
+			public T Deserialize<T>(IDeserializer current, ICfgNode node)
+			{
+				return Parent.Deserialize<T>(current, node);
+			}
+		}
+
 		/// <summary>
 		/// Set custom deserializer
 		/// </summary>
 		/// <typeparam name="T">required type</typeparam>
 		/// <param name="deserialize">deserialize function</param>
-		public void SetDeserializer<T>(Deserialize<T> deserialize)
+		public void SetDeserializer<T>(ChildDeserializeDefinition<T> deserialize)
 		{
 			_funcMap[typeof(T)] = deserialize;
 		}
-
+		
 		/// <summary>
 		/// Set custom deserializer
 		/// </summary>
@@ -35,15 +59,18 @@ namespace NConfiguration.Serialization
 		/// <param name="deserializer">deserializer</param>
 		public void SetDeserializer<T>(IDeserializer<T> deserializer)
 		{
-			_funcMap[typeof(T)] = (Deserialize<T>)deserializer.Deserialize;
+			ChildDeserializeDefinition<T> func = (context, node) => deserializer.Deserialize(context.Current, node);
+			_funcMap[typeof(T)] = func;
 		}
 
 		public T Deserialize<T>(IDeserializer context, ICfgNode node)
 		{
 			object deserialize;
-			if (_funcMap.TryGetValue(typeof(T), out deserialize))
-				return ((Deserialize<T>)deserialize)(_parent, node);
-
+			if (_funcMap.TryGetValue(typeof (T), out deserialize))
+			{
+				return ((ChildDeserializeDefinition<T>)deserialize)(new Context(_parent, context), node);
+			}
+			
 			return _parent.Deserialize<T>(context, node);
 		}
 	}
