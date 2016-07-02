db30143c9dcaf3a50ea98e33b449048268fd9cc9
ExM <ExplorerMachine@gmail.com>
2016-07-02 15:25:01 +0300

    variables prototype

---
+++ NConfiguration/Variables/CfgNodeWrapper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NConfiguration.Variables
+{
+	internal class CfgNodeWrapper: CfgNode
+	{
+		private readonly ICfgNode _wrapped;
+		private readonly VariableStorage _variableStorage;
+
+		public CfgNodeWrapper(ICfgNode wrapped, VariableStorage variableStorage)
+		{
+			_wrapped = wrapped;
+			_variableStorage = variableStorage;
+		}
+
+		public override string GetNodeText()
+		{
+			return new ValueRenderer(_wrapped.Text).Render(_variableStorage);
+		}
+
+		public override IEnumerable<KeyValuePair<string, ICfgNode>> GetNestedNodes()
+		{
+			return _wrapped.Nested.Select(
+				pair => new KeyValuePair<string, ICfgNode>(pair.Key, new CfgNodeWrapper(pair.Value, _variableStorage)));
+		}
+	}
+}
---
+++ NConfiguration/Variables/VariableStorage.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using NConfiguration.Monitoring;
+using NConfiguration.Serialization;
+
+namespace NConfiguration.Variables
+{
+	public class VariableStorage
+	{
+		private readonly Dictionary<string, string> _map = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase);
+
+		public void Set(string name, string value)
+		{
+			if (_map.ContainsKey(name))
+				return;
+
+			_map.Add(name, value);
+		}
+
+		public string Get(string name)
+		{
+			string value;
+			if (_map.TryGetValue(name, out value))
+				return value;
+
+			throw new InvalidOperationException(string.Format("variable '{0}' not found", name));
+		}
+
+		public ICfgNode CfgNodeConverter(string name, ICfgNode candidate)
+		{
+			if (NameComparer.Equals(name, "variable"))
+			{
+				var varConfig = DefaultDeserializer.Instance.Deserialize<VariableConfig>(candidate);
+				Set(varConfig.Name, varConfig.Value);
+				return null;
+			}
+
+			return new CfgNodeWrapper(candidate, this);
+		}
+
+		internal class VariableConfig
+		{
+			[DataMember(Name = "Name", IsRequired = true)]
+			public string Name { get; set; }
+
+			[DataMember(Name = "Value", IsRequired = true)]
+			public string Value { get; set; }
+		}
+	}
+}
